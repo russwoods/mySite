fbc776887f6da802f78f7d452a3cc228699accb4
Russell Woods <rkwoods@gmail.com>
2018-09-04 15:37:00 -0700

    added color field to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: "https://www.pexels.com/photo/bicycle-bike-blue-bricks-1149601", color: "Yellow")
