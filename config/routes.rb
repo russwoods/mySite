11fd0a9db0c13aeee3a089ffbae2ed5604291606
Russell Woods <rkwoods@gmail.com>
2018-09-10 18:00:30 -0700

    Changes to Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,8 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]    
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/index'
-  get 'simple_pages/landing_page'
-  get 'products/index'
-  get 'products/new'
-  get 'products/edit'
-  get 'products/show'  
-  get 'orders/index'    
-  get 'orders/show'      
 #  root 'simple_pages#index'   
   root 'simple_pages#landing_page'    
+  get 'simple_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
