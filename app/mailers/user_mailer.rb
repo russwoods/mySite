dac89ce35809544bbbde9230aea0e28114405989
Russell Woods <rkwoods@gmail.com>
2019-01-03 12:54:15 -0800

    5.3 Fixing the mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "rkwoods@gmail.com"
 
   def contact_form(email, name, message)
   @message = message
@@ -8,11 +8,11 @@ class UserMailer < ApplicationMailer
          subject: "A new contact form message from #{name}")
   end
     
-    def thank_you
-        @name = params[:name]
-        @email = params[:email]
-        @message = params[:message]
-        UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
+#    def thank_you
+#        @name = params[:name]
+#        @email = params[:email]
+#        @message = params[:message]
+#        UserMailer.contact_form(@email, @name, @message).deliver_now
+#    end
     
 end
