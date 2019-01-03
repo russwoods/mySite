07805f5eabc836ed01d205abf13293fb7b1bbad3
Russell Woods <rkwoods@gmail.com>
2019-01-03 13:27:05 -0800

    5.3 Fixing mailer again

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,21 +4,13 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(3)
   end  
     
-#  def thank_you
-#    @name = params[:name]
-#    @email = params[:email]
-#    @message = params[:message]
-#    ActionMailer::Base.mail(from: @email,
-#      to: 'rkwoods@gmail.com',
-#      subject: "A new contact form message from #{@name}",
-#      body: @message).deliver_now
-#  end
     def thank_you
         @name = params[:name]
         @email = params[:email]
         @message = params[:message]
         UserMailer.contact_form(@email, @name, @message).deliver_now
-    end    
+    end
+
      
   def index
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,11 +8,11 @@ class UserMailer < ApplicationMailer
          subject: "A new contact form message from #{name}")
   end
     
-#    def thank_you
-#        @name = params[:name]
-#        @email = params[:email]
-#        @message = params[:message]
-#        UserMailer.contact_form(@email, @name, @message).deliver_now
-#    end
+    def thank_you
+        @name = params[:name]
+        @email = params[:email]
+        @message = params[:message]
+        UserMailer.contact_form(@email, @name, @message).deliver_now
+    end
     
 end
