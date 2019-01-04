01c2f599fe0d623784a2aaac9be0893c87f82b73
Russell Woods <rkwoods@gmail.com>
2019-01-04 13:37:28 -0800

    5.7 Building the comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
+  has_many :orders    
+  has_many :comments    
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end    
 end
