2cba2a8876b2f93b20d32dde9805fabb002e9d53
Russell Woods <rkwoods@gmail.com>
2019-01-04 14:35:56 -0800

    5.8 Added comments controller & views

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,7 @@ class Product < ApplicationRecord
   def lowest_rating_comment
     comments.rating_asc.first
   end    
+  def average_rating
+    comments.average(:rating).to_f
+  end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end      
   resources :orders, only: [:index, :show, :create, :destroy]    
   get 'simple_pages/about'
   get 'simple_pages/contact'
