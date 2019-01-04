f7cedab2b6c12cae2fc9a34a4ad21db7266f0e15
Russell Woods <rkwoods@gmail.com>
2019-01-04 12:59:32 -0800

    5.3 Changes to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,15 +4,15 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   @message = message
     mail(from: email,
-         to: 'your-email@example.com',
+         to: 'rkwoods@gmail.com',
          subject: "A new contact form message from #{name}")
   end
     
-    def thank_you
-        @name = params[:name]
-        @email = params[:email]
-        @message = params[:message]
-        UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
-    
+#    def thank_you
+#        @name = params[:name]
+#        @email = params[:email]
+#        @message = params[:message]
+#        UserMailer.contact_form(@email, @name, @message).deliver_now
+#    end
+#    
 end
