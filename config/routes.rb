24da30792ae6076e6b412a830567a8121d1f5dd4
Russell Woods <rkwoods@gmail.com>
2019-01-05 15:19:33 -0800

    5.10 changes to routes 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :users
   resources :products do
     resources :comments
-  end    
-    
-  resources :users    
-  resources :orders, only: [:index, :show, :create, :destroy] 
-    
+  end      
+  resources :orders, only: [:index, :show, :create, :destroy]    
   get 'simple_pages/about'
-  get 'simple_pages/products'    
   get 'simple_pages/contact'
 #  root 'simple_pages#index'   
   root 'simple_pages#landing_page'    
@@ -17,4 +13,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'simple_pages/thank_you'    
 end
-
