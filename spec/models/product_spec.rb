a8c116fd072e4acef6b430db74c4f69240f9f5a6
Russell Woods <rkwoods@gmail.com>
2019-01-07 17:00:13 -0800

    6.2 Testing using rspec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Product do
+	context "when the product has comments" do
+		let(:product) { Product.create!(name: "race bike") }
+		let(:user) { User.create!(email: "alexander.radloff1@gmail.com", password: "random")}
+        
+		before do
+			product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+			product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+			product.comments.create!(rating: 5, user: user, body: "Great bike!")
+		end
+        
+		it "returns the average rating of all comments" do
+			expect(product.average_rating).to eq 3
+		end
+        
+        it "is not valid without a name" do
+			expect(Product.new(description: "Nice table")).not_to be_valid
+		end
+	end
+end
