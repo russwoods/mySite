82aeb88e8a87e64da12e76e8c51d3ddeed12ccf5
Russell Woods <rkwoods@gmail.com>
2019-01-10 16:55:49 -0800

    5.10 Went back to 5.8 to try and fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,13 @@ class Product < ApplicationRecord
   validates :name, presence: true    
   has_many :orders    
   has_many :comments    
-  def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
-  end
+	def self.search(search_term)
+		if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
+			Product.where("name ilike ?", "%#{search_term}%")
+		else
+			Product.where("name LIKE ?", "%#{search_term}%")
+		end
+	end
   def highest_rating_comment
     comments.rating_desc.first
   end
