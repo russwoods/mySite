e6413b00f472b2715912332c7c1905b127c63fab
Russell Woods <rkwoods@gmail.com>
2019-01-11 14:14:15 -0800

    5.7 Comment Model - 2.1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
-  has_many :comments    
+  has_many :comments, dependent: :destroy
+    
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+    
 end
