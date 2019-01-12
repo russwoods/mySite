ba6a8f31af7e531772cf7756c1d1b3220a15c1cb
Russell Woods <rkwoods@gmail.com>
2019-01-11 17:14:31 -0800

    5.9 Validation & Pagination 2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
     
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true } 
+    
   scope :rating_desc, -> { order(rating: :desc) }    
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true    
   has_many :comments, dependent: :destroy
     
   def self.search(search_term)
