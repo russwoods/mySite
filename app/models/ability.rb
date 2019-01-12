dc30b30db36e31adbdc5a44429e1c6ba1735d0f3
Russell Woods <rkwoods@gmail.com>
2019-01-11 17:53:01 -0800

    5.10 Changed CanCan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,21 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.nil?
+      user = User.new
+    elsif user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+
+      can :manage, Comment.where(user_id: user.id) do |comment|
+        comment.user_id == user.id
+      end
+
+      cannot [:destroy, :delete], Comment.where(user_id: user.id) do |comment|
+        comment.user_id == user.id
+      end
+
+    end
   end
 end
