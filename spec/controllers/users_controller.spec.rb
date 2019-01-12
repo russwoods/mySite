b55b868203ed60cab5e1f3f73389d88e2a702f96
Russell Woods <rkwoods@gmail.com>
2019-01-12 12:12:45 -0800

    6.3 Added rspec testing - 2

---
+++ spec/controllers/users_controller.spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+  let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
+
+  describe 'GET #show' do
+    context 'when a user is logged in' do
+
+      before do
+        sign_in user
+      end
+
+      it "loads correct user details" do
+        get :show, params: {id: user.id}
+        expect(response).to be_ok
+        expect(assigns(:user)).to eq user
+      end
+    end
+
+      context 'when a user is not logged in' do
+        it 'redirects to login' do
+         get :show, params: { id: user.id }
+         expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+  end
+end
