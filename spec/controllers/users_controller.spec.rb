577099c14b983df5abc6dfbd93795668601d14a7
Russell Woods <rkwoods@gmail.com>
2019-01-12 15:05:04 -0800

    6.5 Added stripe - 2

--- spec/controllers/users_controller.spec.rb
+++ spec/controllers/users_controller.spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
+  let(:user) {@user = FactoryBot.create(:user)}
 
   describe 'GET #show' do
     context 'when a user is logged in' do
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,23 +1,22 @@
-require 'rails_helper'
+require 'rails_helper' 
 
 describe Product do
-    context "when the product has comments" do
-        let(:product) { Product.create!(name: "race bike") }
-        let(:user) {User.create!(email: "test@gmail.com", password: "password")}
+	context "when the product has comments" do
+		let(:product) { Product.create!(name: "race bike") }
+		let(:user) { @user = FactoryBot.create(:user) }
 
-        before do
-            product.comments.create!(rating: 1, user: user, body: "Awful bike!")
-            product.comments.create!(rating: 3, user: user, body: "Ok bike!")
-            product.comments.create!(rating: 5, user: user, body: "Great bike!")
-          end
+		before do
+          product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+          product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+          product.comments.create!(rating: 5, user: user, body: "Great bike!")
+        end
 
-          it "returns the average rating of all comments" do
-            expect(product.average_rating).to eq 3.0
-          end
+		it "returns the average rating of all comments" do
+			expect(product.average_rating).to eq 3
+		end
 
-          it "is not valid without a name" do
-            expect(Product.new(description: "Nice bike")).not_to be_valid
-          end
-
-    end
+		it "is not valid without a name" do
+			expect(Product.new(description: "Nice bike")).not_to be_valid
+		end
+	end
 end
