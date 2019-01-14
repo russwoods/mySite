38b3909578e158ae39780745233fcc567aa00d99
Russell Woods <rkwoods@gmail.com>
2019-01-14 13:56:34 -0800

    6.14 Active job & comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,18 @@
 class CommentsController < ApplicationController
+	
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
+		@user = current_user
 
 		respond_to do |format|
 			if @comment.save
-                ActionCable.server.broadcast 'product_channel', comment: "Hello World", average_rating: 5
+#				ProductChannel.broadcast_to @product.id, comment: @comment, average_rating: @product.average_rating
+#				ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
 				format.html { redirect_to @product, notice: 'Review was created successfully.' }
 				format.json { render :show, status: :created, location: @product }
-                format.js
+        format.js
 			else
 				format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
 				format.json { render json: @comment.errors, status: :unprocessable_entity }
